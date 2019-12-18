14b7fa4314b1f61d7619bbd86059992eb997c8cc
Eliza Weisman <eliza@buoyant.io>
2019-12-17 19:02:17 -0800

    bad homemade rwlock actually works now
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/shared.rs
+++ src/shared.rs
@@ -1,6 +1,9 @@
 use crate::loom::{
     cell::CausalCell,
-    sync::atomic::{spin_loop_hint, AtomicUsize, Ordering},
+    sync::{
+        atomic::{spin_loop_hint, AtomicUsize, Ordering},
+        Condvar, Mutex, MutexGuard,
+    },
 };
 use slab::Slab;
 use std::collections::HashMap;
@@ -12,17 +15,20 @@ const READER_ONE: usize = 1 << 2;
 #[derive(Debug)]
 pub(crate) struct Shared<K: Hash + Eq, V> {
     shards: CausalCell<Slab<CausalCell<HashMap<K, V>>>>,
-    lock: AtomicUsize,
+    writer: Mutex<usize>,
+    cv: Condvar,
 }
 
 pub(crate) struct ReadGuard<'a, K: Hash + Eq, V> {
-    lock: &'a AtomicUsize,
     shards: &'a CausalCell<Slab<CausalCell<HashMap<K, V>>>>,
+    writer: &'a Mutex<usize>,
+    cv: &'a Condvar,
 }
 
 #[derive(Debug)]
 pub(crate) struct WriteGuard<'a, K: Hash + Eq, V> {
-    lock: &'a AtomicUsize,
+    cv: &'a Condvar,
+    writer: &'a Mutex<usize>,
     shards: &'a CausalCell<Slab<CausalCell<HashMap<K, V>>>>,
 }
 
@@ -30,63 +36,48 @@ impl<K: Hash + Eq, V> Shared<K, V> {
     pub(crate) fn new() -> Self {
         Self {
             shards: CausalCell::new(Slab::new()),
-            lock: AtomicUsize::new(0),
+            cv: Condvar::new(),
+            writer: Mutex::new(0),
         }
     }
 
     pub(crate) fn read(&self) -> ReadGuard<'_, K, V> {
+        let mut locked = self.writer.lock().unwrap();
         loop {
-            if let Some(read) = self.try_read() {
+            if let Some(read) = self.try_read2(&mut locked) {
                 return read;
             }
-
-            spin_loop_hint();
+            locked = self.cv.wait(locked).unwrap();
         }
     }
 
     pub(crate) fn write(&self) -> WriteGuard<'_, K, V> {
-        loop {
-            match self
-                .lock
-                .compare_exchange_weak(0, LOCKED, Ordering::Acquire, Ordering::Acquire)
-            {
-                Ok(_) => {
-                    println!("ACQUIRE WRITE GUARD");
-                    return WriteGuard {
-                        lock: &self.lock,
-                        shards: &self.shards,
-                    };
-                }
-                Err(actual) => println!("actual = {:?}", actual),
-            }
-            spin_loop_hint();
+        let mut locked = self.writer.lock().unwrap();
+        while *locked > 0 {
+            locked = self.cv.wait(locked).unwrap();
+        }
+        *locked = LOCKED;
+        WriteGuard {
+            shards: &self.shards,
+            cv: &self.cv,
+            writer: &self.writer,
         }
     }
 
     pub(crate) fn try_read(&self) -> Option<ReadGuard<'_, K, V>> {
-        let mut val = self.lock.load(Ordering::Relaxed);
-        loop {
-            if val & LOCKED != 0 {
-                return None;
-            }
+        self.try_read2(&mut self.writer.lock().unwrap())
+    }
 
-            match self.lock.compare_exchange_weak(
-                val,
-                val + READER_ONE,
-                Ordering::Acquire,
-                Ordering::Acquire,
-            ) {
-                Ok(_) => {
-                    return Some(ReadGuard {
-                        lock: &self.lock,
-                        shards: &self.shards,
-                    })
-                }
-                Err(actual) => {
-                    val = actual;
-                }
-            }
+    fn try_read2(&self, guard: &mut MutexGuard<'_, usize>) -> Option<ReadGuard<'_, K, V>> {
+        if (**guard) & LOCKED == 0 {
+            (**guard) += READER_ONE;
+            return Some(ReadGuard {
+                writer: &self.writer,
+                shards: &self.shards,
+                cv: &self.cv,
+            });
         }
+        None
     }
 }
 
@@ -99,7 +90,8 @@ impl<'a, K: Hash + Eq, V> ReadGuard<'a, K, V> {
 
 impl<'a, K: Hash + Eq, V> Drop for ReadGuard<'a, K, V> {
     fn drop(&mut self) {
-        self.lock.fetch_sub(READER_ONE, Ordering::Release);
+        *self.writer.lock().unwrap() -= READER_ONE;
+        self.cv.notify_one();
     }
 }
 
@@ -120,7 +112,7 @@ impl<'a, K: Hash + Eq, V> WriteGuard<'a, K, V> {
 
 impl<'a, K: Hash + Eq, V> Drop for WriteGuard<'a, K, V> {
     fn drop(&mut self) {
-        self.lock.store(0, Ordering::Release);
-        println!("DROP WRITE GUARD");
+        *self.writer.lock().unwrap() = 0;
+        self.cv.notify_all();
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,20 +3,32 @@ use super::*;
 #[test]
 fn fuzz_insert() {
     loom::model(|| {
+        println!("\n ---- ITERATION ---- \n");
         let map = SnapMap::new();
         let mut w1 = map.writer();
         let mut w2 = map.writer();
         let j1 = loom::thread::spawn(move || {
             w1.insert(1, "world");
+
+            println!("t1 insert 1");
             w1.insert(2, "earth");
+
+            println!("t1 insert 2");
         });
         let j2 = loom::thread::spawn(move || {
+            println!("t2 insert 1");
             w2.insert(3, "san francisco");
+
+            println!("t2 insert 1");
             w2.insert(4, "oakland");
         });
-        let snap = map.snapshot();
-        for (_, v) in &snap {
-            println!("hello {}", v);
+        {
+            let snap = map.snapshot();
+            println!("snap");
+            for (_, v) in &snap {
+                println!("hello {}", v);
+            }
+            drop(snap)
         }
         j1.join().unwrap();
         j2.join().unwrap();
