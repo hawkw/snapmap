0b44101fd796532e2f40cef10a52d01a899cccb5
Eliza Weisman <eliza@buoyant.io>
2019-12-19 16:13:38 -0800

    fix removal, add debug printlns
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,38 @@ use std::{borrow::Borrow, collections::HashMap, hash::Hash};
 #[cfg(test)]
 mod tests;
 
+macro_rules! snap_dbg {
+    () => {
+        snap_println!("")
+    };
+    ($val:expr) => {
+        match $val {
+            tmp => {
+                snap_println!("{} = {:#?}", stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    // Trailing comma with single argument is ignored
+    ($val:expr,) => { $crate::snap_dbg!($val) };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::snap_dbg!($val)),+,)
+    };
+}
+
+macro_rules! snap_println {
+    ($($arg:tt)*) => {
+        if cfg!(test) || cfg!(snapmap_debug) {
+            eprintln!("[{}:{}] {} {}",
+                file!(),
+                line!(),
+                std::thread::current().name().unwrap_or("???"),
+                format_args!($($arg)*)
+            );
+        }
+    };
+}
+
 #[derive(Debug, Clone)]
 pub struct SnapMap<K: Hash + Eq, V> {
     shared: Arc<RwLock<Shared<K, V>>>,
@@ -28,7 +60,7 @@ unsafe impl<K: Hash + Eq, V> Sync for SnapMap<K, V> {}
 pub struct Writer<K: Hash + Eq, V> {
     shared: Arc<RwLock<Shared<K, V>>>,
     idx: usize,
-    q: HashMap<K, V>,
+    q: HashMap<K, Option<V>>,
 }
 
 pub(crate) type Shared<K, V> = Slab<CausalCell<HashMap<K, V>>>;
@@ -58,9 +90,13 @@ impl<K: Hash + Eq, V> SnapMap<K, V> {
     }
 }
 
-impl<K: Hash + Eq, V> Writer<K, V> {
+impl<K: Hash + Eq + std::fmt::Debug, V: std::fmt::Debug> Writer<K, V> {
     #[inline(always)]
-    fn with_map<T>(&mut self, f: impl FnOnce(&mut HashMap<K, V>) -> T) -> T {
+    fn with_map<T>(
+        &mut self,
+        f: impl FnOnce(&mut HashMap<K, V>) -> T,
+        or: impl FnOnce(&mut HashMap<K, Option<V>>) -> T,
+    ) -> T {
         let q = &mut self.q;
         match self.shared.try_read() {
             Ok(slab) => slab[self.idx].with_mut(|map| {
@@ -68,7 +104,7 @@ impl<K: Hash + Eq, V> Writer<K, V> {
                 Self::do_sync(q, map);
                 f(map)
             }),
-            Err(TryLockError::WouldBlock) => f(q),
+            Err(TryLockError::WouldBlock) => or(q),
             Err(_) => panic!("lock poisoned"),
         }
     }
@@ -79,21 +115,42 @@ impl<K: Hash + Eq, V> Writer<K, V> {
         slab[self.idx].with_mut(|map| unsafe { Self::do_sync(q, &mut *map) });
     }
 
-    fn do_sync(q: &mut HashMap<K, V>, map: &mut HashMap<K, V>) {
+    fn do_sync(q: &mut HashMap<K, Option<V>>, map: &mut HashMap<K, V>) {
         map.reserve(q.len());
-        map.extend(q.drain());
+        for (k, v) in q.drain() {
+            let k = snap_dbg!(k);
+            if let Some(v) = snap_dbg!(v) {
+                snap_dbg!(map.insert(k, v));
+            } else {
+                snap_dbg!(map.remove(&k));
+            }
+        }
     }
 
     pub fn insert(&mut self, key: K, val: V) -> Option<V> {
-        self.with_map(|map| map.insert(key, val))
+        let q = &mut self.q;
+        match self.shared.try_read() {
+            Ok(slab) => slab[self.idx].with_mut(|map| {
+                let map = unsafe { &mut *map };
+                Self::do_sync(q, map);
+                snap_dbg!(map.insert(key, val))
+            }),
+            Err(TryLockError::WouldBlock) => snap_dbg!(q.insert(key, Some(val)))?,
+            Err(_) => panic!("lock poisoned"),
+        }
     }
 
-    pub fn remove<Q>(&mut self, key: &Q) -> Option<V>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq,
-    {
-        self.with_map(|map| map.remove(key))
+    pub fn remove(&mut self, key: K) -> Option<V> {
+        let q = &mut self.q;
+        match self.shared.try_read() {
+            Ok(slab) => slab[self.idx].with_mut(|map| {
+                let map = unsafe { &mut *map };
+                Self::do_sync(q, map);
+                snap_dbg!(map.remove(&key))
+            }),
+            Err(TryLockError::WouldBlock) => snap_dbg!(q.insert(key, None))?,
+            Err(_) => panic!("lock poisoned"),
+        }
     }
 
     pub fn with_mut<Q, T>(&mut self, key: &Q, f: impl FnOnce(&mut V) -> T) -> Option<T>
@@ -106,7 +163,7 @@ impl<K: Hash + Eq, V> Writer<K, V> {
             Ok(lock) => lock,
             Err(TryLockError::WouldBlock) => {
                 if let Some(q) = q.get_mut(key) {
-                    return Some(f(q));
+                    return Some(f(q.as_mut()?));
                 }
                 self.shared.read().unwrap()
             }
